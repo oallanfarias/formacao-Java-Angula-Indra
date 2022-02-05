5751adb3b9389afa72d569adc4d943b712a1b313
Ricardinho <gmnaarea@gmail.com>
2022-02-05 16:20:01 -0300

    Exercicio concluido

---
+++ controller/src/main/java/com/indracompany/treinamento/wsbase/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://indracompany.com/treinamento/wsbase", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.indracompany.treinamento.wsbase;
