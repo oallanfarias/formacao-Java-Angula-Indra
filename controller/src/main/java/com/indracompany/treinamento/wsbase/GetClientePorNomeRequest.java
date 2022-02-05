5bfc6fd83d701536c22a08a39090ecb608a0def1
Luan Cássio <luancassio2307@gmail.com>
2022-02-05 15:37:56 -0300

    teste 1

---
+++ controller/src/main/java/com/indracompany/treinamento/wsbase/GetClientePorNomeRequest.java
@@ -0,0 +1,27 @@
+package com.indracompany.treinamento.wsbase;
+
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "nome"
+})
+@Getter
+@Setter
+@XmlRootElement(name = "getClientePorNomeRequest")
+public class GetClientePorNomeRequest {
+
+	 @XmlElement(required = true)
+	    private String nome;
+	 			
+	
+}
