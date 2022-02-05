20166cfa429f205ca390a2c5b280f6c1830105af
efmendes <efmendes@minsait.com>
2022-02-05 11:42:55 -0300

    busca de cliente por nome

--- model/src/main/java/com/indracompany/treinamento/model/dto/ClienteDTO.java
+++ model/src/main/java/com/indracompany/treinamento/model/dto/ClienteDTO.java
@@ -11,5 +11,7 @@ public class ClienteDTO {
 	
 	private String email;
 	
+	private String cpf;
+	
 
 }
--- model/src/main/java/com/indracompany/treinamento/model/repository/ClienteRepository.java
+++ model/src/main/java/com/indracompany/treinamento/model/repository/ClienteRepository.java
@@ -1,12 +1,12 @@
 package com.indracompany.treinamento.model.repository;
 
-import org.springframework.data.repository.query.Param;
-
 import com.indracompany.treinamento.model.entity.Cliente;
 
 public interface ClienteRepository extends GenericCrudRepository<Cliente, Long>{
 	
-	public Cliente findByCpf(@Param("cpf")String cpf);
+	public Cliente findByCpf(String cpf);
+
+	public Cliente findByNome(String nome);
 
 	
 
