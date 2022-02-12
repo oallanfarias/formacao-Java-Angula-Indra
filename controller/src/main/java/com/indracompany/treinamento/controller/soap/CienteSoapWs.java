4f16d79e7d10db149e46c696970ad49a0048ee8d
Leonardo Mattos <larrudam@indracompany.com>
2022-02-12 13:09:01 -0300

    Revert "atualizando branch"
    This reverts commit 23caac625b149cfed0b8c7fece7c7daa79670b65.

--- controller/src/main/java/com/indracompany/treinamento/controller/soap/CienteSoapWs.java
+++ controller/src/main/java/com/indracompany/treinamento/controller/soap/CienteSoapWs.java
@@ -11,6 +11,7 @@ import com.indracompany.treinamento.model.dto.ClienteDTO;
 import com.indracompany.treinamento.model.service.ClienteService;
 import com.indracompany.treinamento.wsbase.ClienteWsModel;
 import com.indracompany.treinamento.wsbase.GetClientePorCpfRequest;
+import com.indracompany.treinamento.wsbase.GetClientePorNomeRequest;
 import com.indracompany.treinamento.wsbase.GetClienteResponse;
 
 
@@ -37,5 +38,38 @@ public class CienteSoapWs {
 		return response;
 	}
 
+	
+	
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getClientePorNomeRequest")
+	@ResponsePayload
+	public GetClienteResponse getClientePorNome(@RequestPayload GetClientePorNomeRequest request) {
+		GetClienteResponse response = new GetClienteResponse();
+		System.out.println(request.getNome());
+		ClienteDTO dto = clienteService.buscarClientePorNome(request.getNome());
+		ClienteWsModel model = new ClienteWsModel();
+		
+		BeanUtils.copyProperties(dto, model);
+		
+
+		response.setCliente(model);
+		return response;
+	
+	}
+	
+		@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getClientePorNomeRequest")
+		@ResponsePayload
+		public GetClienteResponse getClientePorNome(@RequestPayload GetClientePorNomeRequest request) {
+			GetClienteResponse response = new GetClienteResponse();
+			
+			ClienteDTO dto = clienteService.buscarClientePornome(request.getNome());
+			ClienteWsModel model = new ClienteWsModel();
+			
+			BeanUtils.copyProperties(dto, model);
+			
+
+			response.setCliente(model);
+			return response;
+		
+		}
 		
 }
